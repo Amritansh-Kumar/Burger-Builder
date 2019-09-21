00c6dd6aa99718d2a1628453e6e26b3176419ba3
Amritansh-Kumar <yadavamritanshami@gmail.com>
2019-09-21 10:29:15 +0530

    added control panel

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './BuildControl.module.css'
+
+interface BuildControlProps {
+    key: string
+    label:string,
+    type:string,
+    addMore:Function,
+}
+
+const buildControl: React.FC<BuildControlProps> = (props: any) => {
+    console.log('sdfsdfsdfsdf');
+
+    return (
+        <div className={classes.BuildControl}>
+            <div className={classes.Label}>{props.label}</div>
+            <button className={classes.Less}>Less</button>
+            <button className={classes.More} onClick={props.addMore}>More</button>
+        </div>
+    )
+};
+
+export default buildControl;
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -1,7 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BurgerControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICING = {
+    salad: 0.5,
+    bacon: 0.8,
+    cheese: 0.7,
+    meat: 1.5
+};
+
+enum BurgerState {
+    salad,
+    bacon,
+    cheese,
+    meat
+}
 
 class BurgerBuilder extends Component {
 
@@ -11,14 +26,30 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     };
 
-    render () {
+    addIngredientHandler = (type: keyof typeof BurgerState) => {
+        const newCount = this.state.ingredients[type] + 1;
+        const updatedIngredients = {...this.state.ingredients };
+        updatedIngredients[type] = newCount;
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICING[type];
+
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    };
+
+
+    removeIngredientHandler = () => {
+
+    };
+
+    render() {
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BurgerControls addIngredient={this.addIngredientHandler}/>
             </Aux>
         );
     }
