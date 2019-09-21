94507a5b8a5c9f47f0ababdaf2484a1ed301f83b
Amritansh-Kumar <yadavamritanshami@gmail.com>
2019-09-21 18:58:35 +0530

    code refactoring

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 interface ModalProps {
@@ -9,21 +9,46 @@ interface ModalProps {
     closeModal: Function
 }
 
-const modal: React.FC<ModalProps> = (props: any) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clickBackdrop={props.closeModal}/>
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? 1 : 0
-                }}
-            >
-                {props.children}
-            </div>
-        </Aux>
-    )
-};
+class Modal extends Component<ModalProps> {
 
-export default modal;
+
+    shouldComponentUpdate(nextProps: Readonly<ModalProps>, nextState: Readonly<{}>, nextContext: any): boolean {
+        return nextProps.show !== this.props.show;
+    }
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clickBackdrop={this.props.closeModal}/>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? 1 : 0
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux>);
+    }
+}
+
+// const modal: React.FC<ModalProps> = (props: any) => {
+//     return (
+//         <Aux>
+//             <Backdrop show={props.show} clickBackdrop={props.closeModal}/>
+//             <div
+//                 className={classes.Modal}
+//                 style={{
+//                     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+//                     opacity: props.show ? 1 : 0
+//                 }}
+//             >
+//                 {props.children}
+//             </div>
+//         </Aux>
+//     )
+// };
+//
+// export default modal;
+export default Modal;
